30c9cc41fee6b328defdaf1b4f69940e2b02201c
jstukenb <jstukenb@lion.lmu.edu>
2021-01-02 13:36:02 -0800

    Updates

---
+++ precache-manifest.88d6761880f15ab63dc101bbe74aba55.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a486cc1d281558cf021783e6b3b22430",
+    "url": "/lol-summer-app/index.html"
+  },
+  {
+    "revision": "0fca10d8f29ea0de60ed",
+    "url": "/lol-summer-app/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "49ec76ba27ad765de752",
+    "url": "/lol-summer-app/static/css/main.934a8677.chunk.css"
+  },
+  {
+    "revision": "0fca10d8f29ea0de60ed",
+    "url": "/lol-summer-app/static/js/2.13553b09.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lol-summer-app/static/js/2.13553b09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49ec76ba27ad765de752",
+    "url": "/lol-summer-app/static/js/main.b8f82104.chunk.js"
+  },
+  {
+    "revision": "aca236b2e4d3efd3ebf6",
+    "url": "/lol-summer-app/static/js/runtime-main.8d7fcbb1.js"
+  }
+]);
