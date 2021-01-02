a6e86a4c6cd19167dd2eb96f92d4a8290e141882
jstukenb <jstukenb@lion.lmu.edu>
2021-01-02 13:46:20 -0800

    fixing cs

--- src/components/MatchHistory/BasicStats.js
+++ src/components/MatchHistory/BasicStats.js
@@ -6,7 +6,7 @@ const BasicStats = props => {
     let assists = props.gameData.participants[props.participantId].stats.assists
     let KDA = kills + "/" + deaths + "/" + assists
     let KDANumber = (kills + assists) / deaths
-
+    let CS = props.gameData.participants[props.participantId].stats.totalMinionsKilled + props.gameData.participants[props.participantId].stats.neutralMinionsKilled
     if (KDANumber === Infinity) {
         KDANumber = "PERFECT"
     } else {
@@ -14,7 +14,7 @@ const BasicStats = props => {
     }
     return(
         <div className = "basicStatsShell">
-            {KDA} KDA: {KDANumber} CS: {props.gameData.participants[props.participantId].stats.totalMinionsKilled}
+            {KDA} KDA: {KDANumber} CS: {CS}
         </div>
     )
 }
